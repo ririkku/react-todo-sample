fb0419bfee1dbc76378fd6058dcdaca2a46b4d68
ririkku <ryoji19950109@gmail.com>
2020-10-09 22:07:58 +0900

    😂

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -19,12 +19,37 @@ const TodoList = () => {
 
     const [todos, setTodo] = useState(initialState);
 
+    const [task, setTask] = useState('');
+
+    const handleNewTask = (event: any) => {
+        setTask(event.target.value);
+    };
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        if (task === '') {
+            return;
+        }
+        setTodo(todos => [...todos, {task: task, isCompleted: false}]);
+        setTask('');
+    };
+
+    const handleRemoveTask = (index: any) => {
+      const newTodos = todos.filter((todo) => {
+          return todo !== todos[index];
+      });
+      setTodo(newTodos);
+    };
+
     return (
         <>
             <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                Add Task : <input value={task} placeholder={"タスク追加するとこ"} onChange={handleNewTask}/>
+            </form>
             <ul>
                 {todos.map((todo, index) => (
-                    <li key={index}>{todo.task}</li>
+                    <li key={index}>{todo.task} <span onClick={() => handleRemoveTask(index)}>[終わったw😂]</span></li>
                 ))}
             </ul>
         </>
